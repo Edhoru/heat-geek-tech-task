ed8455e465b80645eb8728895aa49f778fe3546b
Nov Petrović <novicapetrovic97@gmail.com>
2024-05-01 12:19:44 +0100

    Initial Commit

---
+++ Heat Geek Tech Task/Heat_Geek_Tech_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Heat_Geek_Tech_TaskApp.swift
+//  Heat Geek Tech Task
+//
+//  Created by Nov Petrović on 01/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Heat_Geek_Tech_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
